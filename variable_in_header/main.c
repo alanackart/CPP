be3c7e024c78fb57e5fb80624f84ed4a1fe53142
ChaoF <alanackart@gmail.com>
2018-08-10 08:48:22 +0800

    Add files via upload

---
+++ variable_in_header/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "test.h"
+int main() {
+    printf("Hello, World!a=%d, b=%d\n", a, b);
+    return 0;
+}
---
+++ variable_in_header/test.h
@@ -0,0 +1,9 @@
+//
+// Created by hp on 2018/8/9.
+//
+
+#ifndef UNTITLED_TEST_H
+#define UNTITLED_TEST_H
+#define a 10
+int b= 20;
+#endif //UNTITLED_TEST_H
