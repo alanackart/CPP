4d7389e48d217cb8d10b35814d339151072de9a9
ChaoF <alanackart@gmail.com>
2018-12-23 16:18:33 +0800

    Delete encry_decry.h

--- encry_decry_bak/encry_decry.h
+++
@@ -1,7 +0,0 @@
-#ifndef ENCRY_DECRY_H
-#define ENCRY_DECRY_H
-
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *ciphertext);
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *plaintext);
-
-#endif
